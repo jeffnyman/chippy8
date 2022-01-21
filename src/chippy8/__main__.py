75f3177ae3d3b8a3d1d7c112d1f55a5791708769
Jeff Nyman <jeffnyman@gmail.com>
2022-01-21 03:41:54 -0600

    Addition of startup module.

--- src/chippy8/__main__.py
+++ src/chippy8/__main__.py
@@ -3,12 +3,7 @@
 import sys
 
 
-def main() -> int:
-    """Entry point function for ChipPy-8."""
-
-    print("\nChipPy-8 (CHIP-8 Emulator and Interpreter)\n")
-
-    return 0
+from chippy8.startup import main
 
 
 if __name__ == "__main__":
---
+++ src/chippy8/startup.py
@@ -0,0 +1,9 @@
+"""Entry point module for ChipPy-8."""
+
+
+def main() -> int:
+    """Entry point function for ChipPy-8."""
+
+    print("\nChipPy-8 (CHIP-8 Emulator and Interpreter)\n")
+
+    return 0
